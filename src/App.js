df5dd4abb8ec8c9d570afb94cceca3bf6efb6dd8
receiver@kakao.com <receiver@kakao.com>
2020-12-23 02:58:15 +0900

    go to sleep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    
+    super(props);
+    console.log("hello!")
+    
+  }
+  state = {
+    count :0
+  };
+  add = () => {
+    this.setState( current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState( current => ({ count: current.count - 1 }));
+  };
+  componentDidMount(){
+    console.log("component rednered");
+  }
+  componentDidUpdate(){
+    console.log("i just updated!");
+  }
+  componentWillUnmount(){
+    console.log("bye");
+  }
+  render(){
+    console.log("iamrendered !!")
+    return( 
+    <div>
+      <h1> the number is: {this.state.count}</h1>
+      <button onClick={this.add}> ADD </button>
+      <button onClick = {this.minus}> Minus </button>
+    </div>);
+  }
 }
 
 export default App;
