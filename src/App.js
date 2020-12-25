e26b275b079f72a585f2603fe73fbfa9439e9192
receiver@kakao.com <receiver@kakao.com>
2020-12-25 23:09:59 +0900

    12/25

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
-  constructor(props){
-    
-    super(props);
-    console.log("hello!")
-    
-  }
+  
   state = {
-    count :0
+    isLoading : true,
+    movies: [],
   };
-  add = () => {
-    this.setState( current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState( current => ({ count: current.count - 1 }));
-  };
+  getMovies = async() =>{
+    const {data:{data:{movies}}} = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies,isLoading:false});
+  }
   componentDidMount(){
-    console.log("component rednered");
-  }
-  componentDidUpdate(){
-    console.log("i just updated!");
-  }
-  componentWillUnmount(){
-    console.log("bye");
+     this.getMovies();
+      
   }
   render(){
-    console.log("iamrendered !!")
-    return( 
-    <div>
-      <h1> the number is: {this.state.count}</h1>
-      <button onClick={this.add}> ADD </button>
-      <button onClick = {this.minus}> Minus </button>
-    </div>);
+    const {isLoading,movies} = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie => (
+      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+    ))}
+      </div>
   }
 }
 
