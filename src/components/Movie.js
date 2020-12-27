eb53fd6ba520ef4a778cdd23ec929bf4374bc28c
receiver@kakao.com <receiver@kakao.com>
2020-12-28 06:25:59 +0900

    go to sleep..

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom"
 import PropTypes from "prop-types";
 import "./Movie.css"
 // 
 function Movie({year,title,summary,poster,genres}){
     return (
+    <Link to={{
+        pathname :"/movie-detail",
+        state : {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+        }
+    }}>
     <div className="movie">
         <img src={poster} alt={title}/>
         
@@ -19,6 +30,7 @@ function Movie({year,title,summary,poster,genres}){
          
     </div>
     </div>
+    </Link>
     );
 }
 
